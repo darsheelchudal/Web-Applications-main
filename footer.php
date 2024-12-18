f7f840ac6ef92afa1cee67daf46091f8e1f14004
Darsheel Chudal <darsheelchudal11@gmail.com>
2024-12-18 19:36:54 +0545

    final

--- footer.php
+++ footer.php
@@ -38,7 +38,7 @@
                         <div class="col-6 col-md-2 mb-3">
                             <h5>ADDRESS</h5>
                             <ul class="nav flex-column">
-                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0" style="color: #43766C;"><i class="fa-solid fa-location-crosshairs"></i> Ratnachowk 07 Pokhara</a>
+                                <li class="nav-item mb-2"><a href="#" class="nav-link p-0" style="color: #43766C;"><i class="fa-solid fa-location-crosshairs"></i> Kathmandu Nepal</a>
                                 </li>
                                 <li class="nav-item mb-2"><a href="#" class="nav-link p-0" style="color: #43766C;">Features</a></li>
                                 <li class="nav-item mb-2"><a href="#" class="nav-link p-0" style="color: #43766C;">Pricing</a></li>
